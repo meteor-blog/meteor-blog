5edb91a745e12d6a799bf1dc19a69d0115a9e80a
Ry Walker <ry@rywalker.com>
2013-10-06 21:45:00 -0400

    Hello world page

--- package.js
+++ package.js
@@ -3,4 +3,21 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
+  api.use([
+    'deps',
+    'underscore',
+    'templating',
+    'handlebars',
+    'spark',
+    'session',
+    'coffeescript',
+    'iron-router',
+    'less'
+  ], 'client');
+  api.add_files([
+    'blog.coffee',
+    'router.coffee',
+    'blog.less',
+    'templates/blog.html'
+  ], 'client');
 });
