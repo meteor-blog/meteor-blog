meteor-blog/meteor-blog

commit c224d48ffba5e07c5811efc98fb8865f3773f5bb

Don't use session to store pageLimit, breaks IR 0.7.1. Don't need it anyway.
- Remove subscribe call in tagged route

router.coffee
@@ -31,11 +31,8 @@ Router.map ->
       if Blog.settings.blogIndexTemplate
         @template = Blog.settings.blogIndexTemplate
 
-      if not Session.get('postLimit') and Blog.settings.pageSize
-        Session.set 'postLimit', Blog.settings.pageSize
-
     waitOn: -> [
-      Meteor.subscribe 'posts', Session.get('postLimit')
+      Meteor.subscribe 'posts', Blog.settings.pageSize
       Meteor.subscribe 'authors'
     ]
 
@@ -59,10 +56,6 @@ Router.map ->
     onBeforeAction: ->
       if Blog.settings.blogIndexTemplate
         @template = Blog.settings.blogIndexTemplate
-
-      # Set up our own 'waitOn' here since IR does not atually wait on 'waitOn'
-      # (see https://github.com/EventedMind/iron-router/issues/265).
-      @subscribe('taggedPosts', @params.tag).wait()
 
     waitOn: -> [
       Meteor.subscribe 'taggedPosts', @params.tag

commit c9d789400124a8704d5d9b452f36b8a649f1e671

Fix 'load more' pagination (#44). Broken in c224d48.
- Works around issue with IR 0.7.1
- https://github.com/EventedMind/iron-router/issues/624

router.coffee
@@ -34,10 +34,10 @@ Router.map ->
       if not Session.get('postLimit') and Blog.settings.pageSize
         Session.set 'postLimit', Blog.settings.pageSize
 
-    waitOn: -> [
-      Meteor.subscribe 'posts', Session.get('postLimit')
+      @subscribe('posts', Session.get('postLimit')).wait()
+
+    waitOn: ->
       Meteor.subscribe 'authors'
-    ]
 
     fastRender: true
 
@@ -59,10 +59,6 @@ Router.map ->
     onBeforeAction: ->
       if Blog.settings.blogIndexTemplate
         @template = Blog.settings.blogIndexTemplate
-
-      # Set up our own 'waitOn' here since IR does not atually wait on 'waitOn'
-      # (see https://github.com/EventedMind/iron-router/issues/265).
-      @subscribe('taggedPosts', @params.tag).wait()
 
     waitOn: -> [
       Meteor.subscribe 'taggedPosts', @params.tag