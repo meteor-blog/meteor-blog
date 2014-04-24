meteor-blog/meteor-blog

commit e287a9f4b437069f12d886198d2703b905039a40

Look for author.profile.name as well

collections/post.coffee
@@ -60,7 +60,10 @@ class @Post extends Minimongoid
     author = @author()
 
     if author
-      if author.profile and author.profile.firstName and author.profile.lastName
+      if author.profile.name
+        return author.profile.name
+        
+      else if author.profile and author.profile.firstName and author.profile.lastName
         return "#{author.profile.firstName} #{author.profile.lastName}"
 
       else if author.profile and author.profile.twitter

commit ff6b0e576f264895a1ff12326e33204b82fd528b

Fix bugs caused by e287a9f and d52c3ee

client/boot.coffee
@@ -64,13 +64,14 @@ Meteor.startup ->
 
   # reactive-table depends on just-i18n, which clashes with accounts-t9n. This
   # restores the mapping. See: https://github.com/ecohealthalliance/reactive-table/issues/50
-  T9n.map 'en',
-    reactiveTable:
-      filter: 'Filter'
-      show: 'Show'
-      rowsPerPage: 'rows per page'
-      page: 'Page'
-      of: 'of'
+  if typeof T9n isnt 'undefined'
+    T9n.map 'en',
+      reactiveTable:
+        filter: 'Filter'
+        show: 'Show'
+        rowsPerPage: 'rows per page'
+        page: 'Page'
+        of: 'of'
 
 ################################################################################
 # Register Global Helpers

collections/post.coffee
@@ -56,7 +56,7 @@ class @Post extends Minimongoid
     author = @author()
 
     if author
-      if author.profile.name
+      if author.profile and author.profile.name
         return author.profile.name
         
       else if author.profile and author.profile.firstName and author.profile.lastName