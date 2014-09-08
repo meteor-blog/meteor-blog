meteor-blog/meteor-blog

commit 52e24107c356486c7dcaebfb836e3d26810bf669

Fix bug where post body was not showing up in editor
- Something change in Blaze?

client/views/admin/edit.html
@@ -34,7 +34,7 @@
                     <a href="#" class="html-toggle">HTML</a>
                   </div>
                   {{#with editor}}
-                    {{> UI.dynamic template=this.template data=this.post}}
+                    {{> UI.dynamic template=this.template data=post}}
                   {{/with}}
                   <button type="submit" class="btn btn-default">
                     <i class="fa fa-save"></i> Save

commit f7e764b4a970b274907d33aa8a8878dca3d836df

Fix bug where copy-and-paste did not work (broken by 52e2410)

client/views/admin/edit.coffee
@@ -109,7 +109,7 @@ Template.blogAdminEdit.helpers
 
   editor: ->
     template: Session.get('editorTemplate')
-    post: Session.get('currentPost')
+    post: Session.get('currentPost') || getPost()
 
 setEditMode = (mode) ->
   Session.set('editorTemplate', "#{mode}Editor")

client/views/admin/edit.html
@@ -34,7 +34,7 @@
                     <a href="#" class="html-toggle">HTML</a>
                   </div>
                   {{#with editor}}
-                    {{> UI.dynamic template=this.template data=post}}
+                    {{> UI.dynamic template=this.template data=this.post}}
                   {{/with}}
                   <button type="submit" class="btn btn-default">
                     <i class="fa fa-save"></i> Save