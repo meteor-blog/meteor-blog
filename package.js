ee8742ec789055a1b10012e874df8c781577681e
gerard sychay <hellogerard@gmail.com>
2014-10-07 17:13:44 -0400

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A package that provides a blog at /blog",
-  version: "0.5.9",
+  version: "0.5.10",
   name: "ryw:blog",
   git: "https://github.com/Differential/meteor-blog.git"
 });
