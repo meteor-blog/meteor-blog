804fbea2997e4124d7665015e3e19e0fd2de02a1
gerard sychay <hellogerard@gmail.com>
2015-01-21 17:24:34 -0500

    Specify version constraints for these packages
    - Add new .versions file

--- package.js
+++ package.js
@@ -96,9 +96,9 @@ Package.onUse(function(api) {
     'alanning:roles@1.2.13',
     'meteorhacks:fast-render@2.0.2',
     'meteorhacks:subs-manager@1.2.0',
-    'cfs:standard-packages',
-    'cfs:filesystem',
-    'cfs:s3'
+    'cfs:standard-packages@0.5.3',
+    'cfs:filesystem@0.1.1',
+    'cfs:s3@0.1.1'
   ], both);
 
   // FILES FOR SERVER AND CLIENT
